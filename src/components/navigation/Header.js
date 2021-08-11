dcad827f41b89c4e13e8eff56ab3c89acd7aeb1a
sunnextt <sunnext2010@gmail.com>
2021-08-11 02:23:33 +0100

    add footer

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,30 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import MenuIcon from '@material-ui/icons/Menu';
 import red from '@material-ui/core/colors/red';
 import Drawer from '@material-ui/core/Drawer';
+import { Button, Link } from '@material-ui/core';
+import {
+  createMuiTheme,
+  MuiThemeProvider
+} from "@material-ui/core/styles";
+import lightBlue from '@material-ui/core/colors/lightBlue';
 
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: lightBlue["A700"],
+    },
+  },
+});
 
 
 
 const Header = () => {
 
-  const [open, setOpen] = useState(falsee)
-  const theme = useTheme();
+  const [open, setOpen] = useState(false)
+  // const [close, setClose] = useState("")
   const isMobile = useMediaQuery('(min-width:800px)');
 
   const openDraver = () => {
     setOpen(true)
   }
 
+  const handleClose = () => {
+    setOpen(false)
+  }
+  const onClose = () => false
 
     return (
       <div className="main_header">
-        <Drawer anchor={anchor} open={open} onClose={toggleDrawer(anchor, false)}></Drawer>
+        <React.Fragment >
+          <MuiThemeProvider theme={theme}>
+          <Drawer anchor="top" open={open} onClose={onClose}>
+            <Button variant="contained" color="primary">Home</Button>
+            <Button variant="contained" color="primary">Flight</Button>
+            <Button variant="contained" color="primary">Blogs</Button>
+            <Button variant="contained" color="primary">Contact</Button>
+            <Button variant="contained" color="primary"onClick={handleClose}>X</Button>
+          </Drawer>
+          </MuiThemeProvider>
+        </React.Fragment>
         <div>
           <h1>Ocean Flight</h1>
         </div>
@@ -32,7 +58,7 @@ const Header = () => {
         <a href="#" onClick={openDraver} >
           <MenuIcon style={{ fontSize: 40 , color: red[50] }}/>
         </a>: 
-               <Nav className="navigation">
+          <Nav className="navigation">
           <Nav.Item className="navigation_item">
             <Nav.Link eventKey="1" href="#/home">
               Home
