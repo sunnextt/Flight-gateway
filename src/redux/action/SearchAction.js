1ba7e996cf01c7c5a712e49cea002abf3d9460d9
sunnextt <sunnext2010@gmail.com>
2021-08-16 23:36:53 +0100

    add search action

--- src/redux/action/SearchAction.js
+++ src/redux/action/SearchAction.js
@@ -4,14 +4,17 @@ import { getAmadeusData } from "../../api/amadeus.api";
 
 
 
-const SearchAirport =  search =>  ({
-console.log(search)
- getAmadeusData(search)
+const SearchAirport =  (search) => async (dispatch) => {
 
-});
+    const {source, out} = await getAmadeusData(search)
+    console.log(source, out);
 
-// type: userTypes.SEARCH_AIRPORT,
-// payload: data
+      dispatch({
+    type: userTypes.SEARCH_AIRPORT,
+    payload:  source
+  })
+    
+}
   
 
 export default SearchAirport
