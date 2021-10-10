bb01f6e016e2e8499cf4351ad9b7e7f2c30f1749
sunnextt <sunnext2010@gmail.com>
2021-10-10 19:53:06 +0100

    still working on the project

--- src/api/amadeus.api.js
+++ src/api/amadeus.api.js
@@ -3,13 +3,10 @@ import axios from "axios";
 const CancelToken = axios.CancelToken;
 
 export const getAmadeusData = params => {
-
-  // Destructuring params
   const { keyword = "", page = 0, city = true, airport = true } = params;
 
   const subTypeCheck = city && airport ? "CITY,AIRPORT" : city ? "CITY" : airport ? "AIRPORT" : ""
 
-  // Amadeus API require at least 1 character, so with this we can be sure that we can make this request
   const searchQuery = keyword ? keyword : "a";
 
   const source = CancelToken.source();
